f306024c94be424306a12a6fb121bc83d6ce508c
dpb15 <brannerchinese@gmail.com>
2014-03-04 16:43:03 -0500

    add working format_poem.py but not tests

--- poetry_flask/format_poem.py
+++ poetry_flask/format_poem.py
@@ -1,17 +1,32 @@
+# -*- coding: utf-8 -*-
+# Strangely, encoding declaration is needed under Python 3 here to avoid
+#    "SyntaxError: Non-ASCII character '\xe6'".
+#
 # format_poem.py
 # David Prager Branner
-# 20140303
+# 20140304
 
 import re
 import pprint
 
-def format_poem(poem, to_break_at=['。', '，'], to_strip=None):
+def temp(it):
+    return it
+
+def format_poem(poem, to_break_at=['。', '，', '？'], to_strip=None):
     """Format a Chinese poem. Assume no punctuation, lines are \n-separated."""
+    poem = clean_poem(poem, to_break_at=['。', '，', '？'], to_strip=None)
+    poem = regularize_line_length(poem)
+    poem = rotate_poem(poem)
+    return poem
+
+def clean_poem(poem, to_break_at=['。', '，', '？'], to_strip=None):
     if to_strip == None:
         to_strip = []
-    # Remove whitespace of various kinds, but not ideographic space \u3000.
+    # Remove whitespace of various kinds, but not these:
+    # \n, \r, ideographic space \u3000. Hence \s can't be used here.
     poem = re.sub(r'''\u0020|\u00a0|\u2002|\u2003|\u2004|\u2005|\u2006|'''
-            r'''\u2007|\u2008|\u2009|\u200a|\u200b|\200c|\205f|\t''', '', poem)
+            r'''\u2007|\u2008|\u2009|\u200a|\u200b|\u200c|\u205f|\t''',
+            r'', poem)
     # Replace to_break_at items with linebreaks
     #     and consolidate multiple linebreaks.
     for item in to_break_at:
@@ -19,29 +34,29 @@ def format_poem(poem, to_break_at=['。', '，'], to_strip=None):
         poem = pattern.sub('\n', poem)
     poem = poem.splitlines()
     poem = [
-            [char for char in line if char not in to_strip] 
-            for line in poem]
-#    pprint.pprint(poem)
-    print('\n')
-    # Pad all lines to make them as long as the longest line.
-    longest_length = max([len(line) for line in poem])
-    print(longest_length)
-    # Rotate poem.
-    rotated = []
-    for row in range(len(poem)):
-        line = []
-        for char in range(len(poem)):
-            old_line = poem[len(poem) - 1 - char]
-#            print(row, char, old_line)
-            if row >= len(old_line):
-                continue
-            line.append(poem[len(poem) - 1 - char][row])
-#        print(line)
-        rotated.append(line)
-#    rotated = [
-#            pass
-#            ]
-#    formatted = ['<tr>' + 
-#            '\n'.join(['<td>' + str(char) + '</td>' for char in line])
-#            + '</tr>' for line in poem]
+            [char for char in line if char not in to_strip]
+            for line in poem if any(line)]
+    return poem
+
+def regularize_line_length(poem):
+    """Pad all lines to make them as long as the longest line."""
+    max_len = max([len(line) for line in poem])
+    for i, line in enumerate(poem):
+        poem[i].extend(['\u3000'] * (max_len - len(line)))
+    return poem
+
+def rotate_poem(poem):
+    # Create empty matrix.
+    rows = len(poem) # this many column-elements
+    cols = len(poem[0]) # in each of this many row-lists
+    rotated = [
+                [[] for i in range(rows)]
+                    for i in range(cols)]
+    for row in range(rows):
+        for char in range(cols):
+            rotated[char][rows - 1 - row] = poem[row][char]
     return rotated
+
+##    formatted = ['<tr>' +
+##            '\n'.join(['<td>' + str(char) + '</td>' for char in line])
+##            + '</tr>' for line in poem]
