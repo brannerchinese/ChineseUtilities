1e641b3345d77f37f00c92e16433854676d8c53d
dpb15 <brannerchinese@gmail.com>
2014-03-18 17:04:03 -0400

    remove statistics/ (moved elsewhere)

--- statistics/chernoff_bound.py
+++
@@ -1,5 +0,0 @@
-def figure_confidence(matches, trials, exp_avg = 0.49672, round_to=2):
-    mu = exp_avg * trials
-    epsilon = abs(matches - mu) / float(mu)
-    confidence = ((M.exp(epsilon) / ((1 + epsilon) ** (1 + epsilon)))) ** mu
-    return abs(round(confidence, round_to))
