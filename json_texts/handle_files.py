811b6f741ba8a6265d63c5fc1e0f7ccf182710e6
David Branner <brannerchinese@gmail.com>
2014-08-10 21:53:49 -0400

    add skeleton `name_rhyming_tone` and change var name

--- json_texts/handle_files.py
+++ json_texts/handle_files.py
@@ -9,22 +9,21 @@ import json
 import subprocess
 import os
 
-
 def retrieve_data(filename='ban_guh_hannshu_shiuhjuann_shiah.ignore.gpg'):
     """Decrypt and return JSON file of working data."""
     with open('phrase.ignore', 'r') as f:
         phrase = f.read().strip('\n')
     subprocess.call(['gpg', '-o', 'temp.ignore', '--passphrase', phrase, 
             '-d', filename])
-    t = json_load('temp.ignore')
+    text = json_load('temp.ignore')
     os.remove('temp.ignore')
-    return t
+    return text
 
-def store_data(t, filename='ban_guh_hannshu_shiuhjuann_shiah.ignore'):
+def store_data(text, filename='ban_guh_hannshu_shiuhjuann_shiah.ignore'):
     """Save and encrypt JSON file of working data."""
-    if not t:
-        t = json_load(filename)
-    content = json.dumps(t)
+    if not text:
+        text = json_load(filename)
+    content = json.dumps(text)
     with open('phrase.ignore', 'r') as f:
         phrase = f.read().strip('\n')
     with open('temp.ignore', 'w') as f:
@@ -37,5 +36,5 @@ def json_load(filename):
     """Load JSON data from file."""
     with open(filename, 'r') as f:
         content = f.read()
-        t = json.loads(content)
-    return t
+        text = json.loads(content)
+    return text
