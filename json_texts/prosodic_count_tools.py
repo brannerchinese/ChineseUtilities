d1fa7060c6020259882261ce0098acf3bc96f34e
David Branner <brannerchinese@gmail.com>
2014-08-10 21:36:55 -0400

    change prosodic_count_tools to use class

--- json_texts/prosodic_count_tools.py
+++ json_texts/prosodic_count_tools.py
@@ -3,52 +3,58 @@
 # David Prager Branner
 # 20140810, works
 
+"""Provide methods for counting number of syllables and lines in poetry."""
+
 import re
 
-def count_syllables(t, feature, prosodic_type='rhyme'):
-    count = 0
-    for section in t:
-        for line in section["section"]["lines"]:
-            if ("prosodic_type" in line and 
-                    "transc" in line and
-                    line["prosodic_type"] == prosodic_type and 
-                    re.search(feature, line["transc"][-1])):
-#                print(line["transc"][-1])
-                count += 1
-    return count
+class Counter():
+    def __init__(self, t):
+        self.t = t
+        self.count = 0
+        self.tones = {
+                'pyng': '[^ptkQH]-',
+                'shaang': '[Q]-',
+                'chiuh': '[H]-',
+                'ruh': '[ptk]-'
+                }
+        self.states = ['rhyme', 'nonrhyme']
 
-def find_all_tone_counts(t):
-    all_tone_count = {}
-    pyng = '[^ptkQH]-'
-    shaang = '[Q]-'
-    chiuh = '[H]-'
-    ruh = '[ptk]-'
-    all_tone_count[('pyng', 'rhyme')] = count_syllables(t, pyng, 'rhyme')
-    all_tone_count[('pyng', 'nonrhyme')] = count_syllables(t, pyng, 'nonrhyme')
-    all_tone_count[('shaang', 'rhyme')] = count_syllables(t, shaang, 'rhyme')
-    all_tone_count[('shaang', 'nonrhyme')] = count_syllables(
-            t, shaang, 'nonrhyme')
-    all_tone_count[('chiuh', 'rhyme')] = count_syllables(t, chiuh, 'rhyme')
-    all_tone_count[('chiuh', 'nonrhyme')] = count_syllables(
-            t, chiuh, 'nonrhyme')
-    all_tone_count[('ruh', 'rhyme')] = count_syllables(t, ruh, 'rhyme')
-    all_tone_count[('ruh', 'nonrhyme')] = count_syllables(t, ruh, 'nonrhyme')
-    return all_tone_count
-
-def count_poetic_lines(t):
-    count = 0
-    for section in t:
-        for line in section["section"]["lines"]:
-            if (line["text_type"] == 'verse' and 
-                    (line["prosodic_type"] == 'rhyme' or 
-                    line["prosodic_type"] == 'nonrhyme')):
-#                print(line["transc"][-1])
-                count += 1
-        print('end of lines')
-    return count
-
-def count_shanqwoei():
-    # In a given section,
-    # decide the rhyming tone;
-    # count the cases of that tone in nonrhyme lines.
-    pass
+    def count_sylls(self, feature, prosodic_type='rhyme'):
+        """Return number of syllables of a specific type, as integer."""
+        self.count = 0
+        for section in self.t:
+            for line in section["section"]["lines"]:
+                if ("prosodic_type" in line and 
+                        "transc" in line and
+                        line["prosodic_type"] == prosodic_type and 
+                        re.search(feature, line["transc"][-1])):
+    #                print(line["transc"][-1])
+                    self.count += 1
+        return self.count
+    
+    def find_all_tones(self):
+        """Return dictionary of all tone-counts, rhyming and non-rhyming."""
+        all_tones = {
+                (tone, state): self.count_sylls(self.tones[tone], state) 
+                for tone in self.tones 
+                for state in self.states
+                }
+        return all_tones
+    
+    def count_poetic_lines(self):
+        """Return number of rhyme and nonrhyme lines, as integer."""
+        self.count = 0
+        for section in self.t:
+            for line in section["section"]["lines"]:
+                if (line["text_type"] == 'verse' and 
+                        (line["prosodic_type"] == 'rhyme' or 
+                        line["prosodic_type"] == 'nonrhyme')):
+    #                print(line["transc"][-1])
+                    self.count += 1
+        return self.count
+    
+    def count_shanqwoei(self):
+        # In a given section,
+        # decide the rhyming tone;
+        # count the cases of that tone in nonrhyme lines.
+        pass
